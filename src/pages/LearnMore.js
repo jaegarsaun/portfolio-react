5d9b53c0e3874f3a5aaffeb001ae29454caf4c47
Jaegar <jaegarls@icloud.com>
2023-06-12 16:52:00 -0230

    Found new icons, added animations, added bio

--- src/pages/LearnMore.js
+++ src/pages/LearnMore.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import '../styles.css';
 import csgoFragsImg from '../imgs/csgofrags.png';
+import { useEffect } from 'react';
+// import svgs
+
+import { ReactComponent as GithubSVG } from '../imgs/svg/github.svg';
+import { ReactComponent as GitSVG } from '../imgs/svg/git.svg';
+import { ReactComponent as FigmaSVG } from '../imgs/svg/figma.svg';
+import { ReactComponent as PhotoshopSVG } from '../imgs/svg/photoshop.svg';
+import { ReactComponent as VisualStudioSVG } from '../imgs/svg/vscode.svg';
+import { ReactComponent as SQLSVG } from '../imgs/svg/sql.svg';
+import { ReactComponent as NodeSVG } from '../imgs/svg/node.svg';
+import { ReactComponent as ExpressSVG } from '../imgs/svg/express.svg';
+import { ReactComponent as MongoDBSVG } from '../imgs/svg/mongodb.svg';
+import { ReactComponent as ReactSVG } from '../imgs/svg/react.svg';
+import { ReactComponent as ChakraUI } from '../imgs/svg/chakraui.svg';
+import { ReactComponent as JavascriptSVG } from '../imgs/svg/javascript.svg';
+import { ReactComponent as HTMLSVG } from '../imgs/svg/html.svg';
+import { ReactComponent as CSSSVG } from '../imgs/svg/css.svg';
+import { ReactComponent as IntelliJSVG } from '../imgs/svg/intellij.svg';
+import { ReactComponent as ElectronSVG } from '../imgs/svg/electron.svg';
+import { ReactComponent as JavaSVG } from '../imgs/svg/java.svg';
+
+
 
 import anime from 'animejs/lib/anime.es.js';
 
@@ -11,41 +33,54 @@ function LearnMore() {
 
     const successTools = [
         // An array of SVGs
-
-        `<svg viewBox="0 0 128 128">
-        <g fill="#181616"><path fill-rule="evenodd" clip-rule="evenodd" d="M64 5.103c-33.347 0-60.388 27.035-60.388 60.388 0 26.682 17.303 49.317 41.297 57.303 3.017.56 4.125-1.31 4.125-2.905 0-1.44-.056-6.197-.082-11.243-16.8 3.653-20.345-7.125-20.345-7.125-2.747-6.98-6.705-8.836-6.705-8.836-5.48-3.748.413-3.67.413-3.67 6.063.425 9.257 6.223 9.257 6.223 5.386 9.23 14.127 6.562 17.573 5.02.542-3.903 2.107-6.568 3.834-8.076-13.413-1.525-27.514-6.704-27.514-29.843 0-6.593 2.36-11.98 6.223-16.21-.628-1.52-2.695-7.662.584-15.98 0 0 5.07-1.623 16.61 6.19C53.7 35 58.867 34.327 64 34.304c5.13.023 10.3.694 15.127 2.033 11.526-7.813 16.59-6.19 16.59-6.19 3.287 8.317 1.22 14.46.593 15.98 3.872 4.23 6.215 9.617 6.215 16.21 0 23.194-14.127 28.3-27.574 29.796 2.167 1.874 4.097 5.55 4.097 11.183 0 8.08-.07 14.583-.07 16.572 0 1.607 1.088 3.49 4.148 2.897 23.98-7.994 41.263-30.622 41.263-57.294C124.388 32.14 97.35 5.104 64 5.104z"></path><path d="M26.484 91.806c-.133.3-.605.39-1.035.185-.44-.196-.685-.605-.543-.906.13-.31.603-.395 1.04-.188.44.197.69.61.537.91zm2.446 2.729c-.287.267-.85.143-1.232-.28-.396-.42-.47-.983-.177-1.254.298-.266.844-.14 1.24.28.394.426.472.984.17 1.255zM31.312 98.012c-.37.258-.976.017-1.35-.52-.37-.538-.37-1.183.01-1.44.373-.258.97-.025 1.35.507.368.545.368 1.19-.01 1.452zm3.261 3.361c-.33.365-1.036.267-1.552-.23-.527-.487-.674-1.18-.343-1.544.336-.366 1.045-.264 1.564.23.527.486.686 1.18.333 1.543zm4.5 1.951c-.147.473-.825.688-1.51.486-.683-.207-1.13-.76-.99-1.238.14-.477.823-.7 1.512-.485.683.206 1.13.756.988 1.237zm4.943.361c.017.498-.563.91-1.28.92-.723.017-1.308-.387-1.315-.877 0-.503.568-.91 1.29-.924.717-.013 1.306.387 1.306.88zm4.598-.782c.086.485-.413.984-1.126 1.117-.7.13-1.35-.172-1.44-.653-.086-.498.422-.997 1.122-1.126.714-.123 1.354.17 1.444.663zm0 0"></path></g>
-        </svg>`,
-
-        /*
-        React
-        Node
-        MongoDB
-        Express
-        Javascript
-        HTML
-        CSS
-        Electron
-        Chakra UI
-        MongoDB
-        Mongoose
-        SQLite
-        Github
-        Git
-        Figma
-        Photoshop
-        Visual Studio
-
-        
-        */
-
-
+        <GithubSVG />,
+        <GitSVG />,
+        <FigmaSVG />,
+        <PhotoshopSVG />,
+        <VisualStudioSVG />,
+        <SQLSVG />,
+        <NodeSVG />,
+        <ExpressSVG />,
+        <MongoDBSVG />,
+        <ReactSVG />,
+        <JavascriptSVG />,
+        <HTMLSVG />,
+        <CSSSVG />,
+        <IntelliJSVG />,
+        <ElectronSVG />,
+        <JavaSVG />,
+        <ChakraUI />,
     ]
 
-    const toolList = successTools.map((tool, index) => (
-        <li style={styles.toolLi} key={index}>
-            <object data={tool} type="image/svg+xml" style={styles.toolImg} width="48" height="48" />
-        </li>
-    ));
+    const toolList = successTools.map((tool, index) => {
+        // Render each SVG
+        return (
+            <li style={styles.toolLi} key={index} className='svg'>
+                {tool}
+            </li>
+        )
+
+    });
+
+    useEffect(() => {
+        // Run the animation after the component has rendered
+        // Loop the animation
+        anime({
+            targets: '.svg',
+            scale: [
+              {value: .1, easing: 'easeOutSine', duration: 900},
+              {value: 1, easing: 'easeInOutQuad', duration: 1200}
+            ],
+            delay: anime.stagger(500, {grid: [14, 5], from: 'center'}),
+            alternate: true,
+            loop: true,
+            });
+        
+    }, [toolList]); // Run the animation whenever the toolList changes
+
+
+
+
 
 
 
@@ -81,7 +116,12 @@ function LearnMore() {
                     <a href='https://github.com' className='font-reg txt-secondary primary-a-button'>View all projects</a>
                 </section>
                 <section style={styles.section}>
-                    <SectionHead title={'Tools for success'} type='right' />
+                    <SectionHead title={'About Me'} type='right' />
+                    <div style={styles.bio}>
+                        <p className='font-reg txt-secondary' style={styles.bioTxt}>
+                            Hi, I'm <span className='clr-text font-bold'>Jaegar</span>, a passionate IT Programming student with a focus on <span className='clr-text font-bold'>frontend development</span>. I love building and designing web applications. I'm currently studying at <span className='clr-text font-bold'>Nova Scotia Community College</span> and always eager to work on new projects. Graduating from high school early, I've received recognition for my <span className='clr-text font-bold'>dedication and enthusiasm</span>. Let's create something amazing together!
+                        </p>
+                    </div>
                     <div style={styles.hstack}>
                         <div style={styles.left}>
                             <h1 className='font-reg main-txt' style={styles.bigtext}>The tools I use to <br />succeed in my field.</h1>
@@ -89,9 +129,11 @@ function LearnMore() {
                         <div style={styles.right}>
                             <ul style={styles.toolUl}>
                                 {toolList}
+
                             </ul>
                         </div>
                     </div>
+
                 </section>
             </main>
             <div className="app-background"></div>
@@ -145,15 +187,15 @@ const styles = {
         display: 'flex',
         flexWrap: 'wrap',
         width: '100%',
-        justifyContent: 'flex-start',
-        alignItems: 'flex-start',
+        justifyContent: 'center',
+        alignItems: 'center',
         listStyle: 'none',
     },
     toolLi: {
         // margin right, top and bottom 10px
-        marginRight: '10px',
+        marginRight: '15px',
         marginTop: '10px',
-        marginBottom: '10px',
+        marginBottom: '15px',
 
     },
     left: {
@@ -161,6 +203,15 @@ const styles = {
     },
     right: {
         width: '50%',
+    },
+    bio: {
+        width: '100%',
+        marginBottom: '3rem',
+    },
+    bioTxt: {
+        fontSize: '1.25rem',
+        lineHeight: '1.75rem',
+        textAlign: 'center',
     },
 
 }
